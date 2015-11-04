c3a4a9c29600dc8ed5443844a94bbf1d8322a4d7
m-bigglesworth <arnaudgarnier06@gmail.com>
2015-11-04 11:31:36 +0100

    Suppression des warnings dans Requete.java

--- src/serveur/Requete.java
+++ src/serveur/Requete.java
@@ -2,8 +2,6 @@ package serveur;
 
 import java.util.*;
 
-import exception.SurnomDejaExistant;
-
 public interface Requete {
 	public ArrayList<Personne> process(ArrayList<Personne> l1) throws Exception;
 }
