86db34c2b09dfab4638e079fe25b5b0ba059dd99
m-bigglesworth <arnaudgarnier06@gmail.com>
2015-11-04 11:29:28 +0100

    Ajout des serial ID pour les exceptions

--- src/exception/NomNonAttribue.java
+++ src/exception/NomNonAttribue.java
@@ -2,6 +2,11 @@ package exception;
 
 public class NomNonAttribue extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -816352165336575609L;
+
 	public NomNonAttribue() {
 		System.err.println("Ce nom n'est pas attribué.");
 	}
--- src/exception/SurnomDejaExistant.java
+++ src/exception/SurnomDejaExistant.java
@@ -2,6 +2,11 @@ package exception;
 
 public class SurnomDejaExistant extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -32473973890863542L;
+
 	public SurnomDejaExistant() {
 		System.err.println("Ce surnom est deja pris !");
 	}
