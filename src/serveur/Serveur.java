1414a9a64ad300817ee151b414ea3083f4bf1a4d
Lisa <Lisa Joanno>
2015-11-04 11:15:54 +0100

    Serveur

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -3,28 +3,25 @@ package serveur;
 import java.util.ArrayList;
 
 public class Serveur {
-	private ArrayList<Personne> listeSurnoms;
-	
-	/**
-	 * @return the listeSurnoms
-	 */
-	public ArrayList<Personne> getListeSurnoms() {
-		return listeSurnoms;
-	}
-	
-	/**
-	 * @param listeSurnoms the listeSurnoms to set
-	 */
-	public void setListeSurnoms(ArrayList<Personne> listeSurnoms) {
-		this.listeSurnoms = listeSurnoms;
-	}
-	
+	private ArrayList<Personne> listePersonnes;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	/**
+	 * @return the listePersonnes
+	 */
+	public ArrayList<Personne> getListePersonnes() {
+		return listePersonnes;
+	}
+	/**
+	 * @param listePersonnes the listePersonnes to set
+	 */
+	public void setListePersonnes(ArrayList<Personne> listePersonnes) {
+		this.listePersonnes = listePersonnes;
 	}
 
 }
