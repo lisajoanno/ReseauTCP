4668a4518c29282dba659c3c332d00a53133db91
Lisa <Lisa Joanno>
2015-11-04 10:48:30 +0100

    Personne et Serveur pour compilation

---
+++ src/serveur/Personne.java
@@ -0,0 +1,9 @@
+package serveur;
+
+import java.util.List;
+
+public class Personne {
+	private String nom;
+	private List<String> surnoms;
+
+}
---
+++ src/serveur/Serveur.java
@@ -0,0 +1,15 @@
+package serveur;
+
+import java.util.List;
+
+public class Serveur {
+	private List<Personne> listeSurnoms;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
