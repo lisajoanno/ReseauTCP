d385935b4c77f399b99ba141e04471fb08c5e7a4
m-bigglesworth <arnaudgarnier06@gmail.com>
2015-11-13 14:53:54 +0100

    OK GOOGLE

--- src/serveur/ListerPersonne.java
+++ src/serveur/ListerPersonne.java
@@ -11,11 +11,6 @@ public class ListerPersonne implements Requete, Serializable {
 	private static final long serialVersionUID = 1178456591485909596L;
 
 	public BaseDeDonnees process(BaseDeDonnees bd) {
-		System.out.println("Liste des utilisateurs : ");
-		for (Personne personne : bd.getListePersonnes()) {
-			System.out.println("Nom : " + personne.getNom() + " --- Pseudo : "
-					+ personne.getSurnoms());
-		}
 		return bd;
 	}
 
